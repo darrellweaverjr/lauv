336f39b98a08604a8297eb50413af672864382d0
Darrell Weaver <darrellweaverjr@gmail.com>
2020-02-21 12:13:31 -0800

    removed carrot

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ require dirname(__FILE__).'/_dynamix.php';
 	<!-- Google Tag Manager (noscript) -->
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $profile->xid("gtm") ?>"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->>
+	<!-- End Google Tag Manager (noscript) -->
 
 	<section class="contesttop">
 		<div class="hero-header">
