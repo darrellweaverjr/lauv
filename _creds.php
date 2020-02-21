3f505b35c3f3be8db498030c85925d57d6cd7acb
Darrell Weaver <darrellweaverjr@gmail.com>
2020-02-21 12:02:17 -0800

    updated profile names

--- _creds.php
+++ _creds.php
@@ -8,7 +8,7 @@ $api_email    = "thomas.fiss@kobaltmusic.com";
 $dynamixkey   = "0oZRe_8e155169cf35f4e8e483e187a62f67bc12182847";
 $glowfishkey  = "mB7pY_53b6f78ce75f0b7a262cc36770f1d9f53d13b7a3";
 $templates = array(
-  "index"     =>  "lander",
-  "policy"    =>  "policy",
-  "thank-you" =>  "thank-you"
+  "index"     =>  "hm-lander",
+  "policy"    =>  "hm-policy",
+  "thank-you" =>  "hm-thank-you"
 );
