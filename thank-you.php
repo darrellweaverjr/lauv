4f951667a6e2c2be009ff757add9afb0cf9129e3
Darrell Weaver <darrellweaverjr@gmail.com>
2020-02-21 12:16:22 -0800

    thank you

--- thank-you.php
+++ thank-you.php
@@ -58,83 +58,23 @@ require dirname(__FILE__).'/_dynamix.php';
 					<div class="rainbow-bg">
 						<div class="fam-form">
 
-	            <div class="formContent">
-	              <h3 class="header2" data-xid="header2">
+							<div class="formContent">
+								<h3 class="header2" data-xid="header2">
 									<?= $profile->xid("header2") ?>
-	              </h3>
-	            </div>
-							<div class="formblock">
+								</h3>
+							</div>
 
-	              <div class="service-container">
+							<div class="form-item submit-item handm-button" data-xid="H&Mbutton">
+								<?= $profile->xid("H&Mbutton") ?>
+							</div>
 
-	                 <div class="service">
-	                     <a data-xid="apple-music-link" class="img-btn redirect" href="https://music.apple.com/us/artist/982612996?uo=4&amp;app=music&amp;at=1010lpyZ&amp;ct=LFV_83d8003f186e77e39c08fbdf7fa86a6b" data-player="applemusic" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("apple-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="spotify-music-link" class="img-btn redirect" href="https://open.spotify.com/artist/5JZ7CnR6gTvEMKX4g70Amv" data-uri="spotify:artist:5JZ7CnR6gTvEMKX4g70Amv" data-player="spotify" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("spotify-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="itunes-music-link" class="img-btn redirect" href="https://itunes.apple.com/us/artist/1354803196?uo=4&amp;app=itunes&amp;at=1010lpyZ&amp;ct=LFV_83d8003f186e77e39c08fbdf7fa86a6b" data-player="itunes" data-servicetype="download" data-apptype="manual">
-												 <?= $profile->xid("itunes-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="amazon-music-link" class="img-btn redirect" href="https://music.amazon.com/artists/B00CN49HZU?ref=dm_sh_865c-2082-dmcp-c1b3-dcda7&amp;musicTerritory=US&amp;marketplaceId=ATVPDKIKX0DER" data-player="amazonmusic" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("amazon-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="youtube-music-link" class="img-btn redirect" href="https://music.youtube.com/channel/UCWjoDY2SXJ5dvcdunWI6mjQ" data-player="youtubemusic" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("youtube-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="tidal-music-link" class="img-btn redirect" href="https://tidal.com/browse/artist/4898953" data-player="tidal" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("tidal-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="pandora-music-link" class="img-btn redirect" href="https://www.pandora.com/artist/lauv/AR2J3gdxqXvfbww?part=lf&amp;corr=83d8003f186e77e39c08fbdf7fa86a6b" data-player="pandora" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("pandora-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="deezer-music-link" class="img-btn redirect" href="http://www.deezer.com/artist/4677655?app_id=140685" data-player="deezer" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("deezer-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <div class="service">
-	                     <a data-xid="napster-music-link" class="img-btn redirect" href="https://us.napster.com/artist/lauv" data-player="napster" data-servicetype="play" data-apptype="manual">
-												 <?= $profile->xid("napster-music-link") ?>
-	                     </a>
-	                 </div>
-	                 <!--Last option should be 'i dont know'-->
-
-	              </div>
-
-	            </div>
 
 						</div>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="small-12 medium-12 large-8 col-md-offset-2 columns formcontent">
-          <div class="video-sect" data-xid="logo">
-            <div class="video-container" data-xid="youtube">
-							<?= $profile->xid("youtube") ?>
-            </div>
-          </div>
-        </div>
-      </div>
+					</div>
+				</div>
+			</div>
 		</div>
-
-    </section>
+	</section>
 
     <section class="footer" id="footer">
       <div class="footer-content" data-xid="footer-content"><!-- Replace the 'href' value, and the inner HTML link text -->
